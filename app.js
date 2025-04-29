2fa28aab4641327316cd119a9741ab7233bc10aa
Shubham Tiwari <meshubhamtiwarji@gmail.com>
2025-04-29 19:10:47 +0530

    redis connection removed

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ global.OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
-const Redis = require('ioredis');
 const winston = require('winston');
 const cors = require('cors');
 
@@ -13,18 +12,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
-// Redis Setup
-const redis = new Redis(process.env.REDIS_URL);
-
-redis.on('connect', () => {
-  console.log('Redis Connected Successfully');
-});
-
-redis.on('error', (err) => {
-  console.error('Redis Connection Error:', err);
-});
-
-app.set('redis', redis);
 
 // MongoDB Setup
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
